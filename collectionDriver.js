a741487bf5d8992fa7a2d302485a851ead01724a
arloistale <jonathanlu321@gmail.com>
2016-03-02 08:43:28 -0800

    Initial commit with mongodb set up

---
+++ collectionDriver.js
@@ -0,0 +1,101 @@
+var ObjectID = require('mongodb').ObjectID;
+
+CollectionDriver = function(db) {
+    this.db = db;
+};
+
+CollectionDriver.prototype.getCollection = function(collectionName, callback) {
+    this.db.collection(collectionName, function(error, collection) {
+        if(error)
+            callback(error);
+        else
+            callback(null, collection);
+    });
+};
+
+CollectionDriver.prototype.findAll = function(collectionName, callback) {
+    this.getCollection(collectionName, function(error, collection) {
+        if(error)
+            callback(error);
+        else {
+            collection.find().toArray(function(error, results) {
+                if(error)
+                    callback(error);
+                else
+                    callback(null, results);
+            });
+        }
+    });
+};
+
+CollectionDriver.prototype.get = function(collectionName, id, callback) {
+    this.getCollection(collectionName, function(error, collection) {
+        if(error)
+            callback(error);
+        else {
+            var hexCheck = new RegExp("^[0-9a-fA-F]{24}$");
+            if(!hexCheck.test(id)) {
+                callback({
+                    error: "Invalid ID"
+                });
+            } else {
+                collection.findOne({
+                    '_id': ObjectID(id)
+                }, function(error, result) {
+                    if(error)
+                        callback(error);
+                    else
+                        callback(null, result);
+                });
+            }
+        }
+    });
+};
+
+CollectionDriver.prototype.save = function(collectionName, obj, callback) {
+    this.getCollection(collectionName, function(error, collection) {
+        if(error)
+            callback(error);
+        else {
+            obj.created_at = new Date();
+            collection.insert(obj, function() {
+                callback(null, obj);
+            });
+        }
+    });
+};
+
+CollectionDriver.prototype.update = function(collectionName, obj, id, callback) {
+    this.getCollection(collectionName, function(error, collection) {
+        if(error)
+            callback(error);
+        else {
+            obj._id = ObjectID(id); // set new object as the updated object
+            obj.updated_at = new Date();
+            collection.save(obj, function(error, result) {
+                if(error)
+                    callback(error);
+                else
+                    callback(null, result);
+            });
+        }
+    });
+};
+
+CollectionDriver.prototype.delete = function(collectionName, id, callback) {
+    this.getCollection(collectionName, function(error, collection) {
+        if(error)
+            callback(error);
+        else {
+            collection.remove({ '_id': id }, function(error, result) {
+                if(error)
+                    callback(error);
+                else {
+                    callback(result);
+                }
+            });
+        }
+    });
+};
+
+exports.CollectionDriver = CollectionDriver;
---
+++ server.js
@@ -0,0 +1,136 @@
+var express = require('express'),
+    path = require('path'),
+    mongodb = require('mongodb');
+
+MongoClient = mongodb.MongoClient;
+Server = mongodb.Server;
+CollectionDriver = require('./collectionDriver').CollectionDriver;
+
+// app definitions
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// set up mongodb
+var mongoHost = 'localHost';
+var mongoPort = 27017;
+var collectionDriver;
+
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort));
+mongoClient.open(function(err, client) {
+    if(!client) {
+        console.error("Start Mongo first!");
+        process.exit(1);
+    }
+
+    var db = client.db("MainDatabase");
+    collectionDriver = new CollectionDriver(db);
+});
+
+// configure app
+app.use(express.bodyParser());
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/:collection', function(req, res) {
+    var params = req.params;
+    var collection = req.params.collection;
+
+    collectionDriver.findAll(collection, function(error, results) {
+        if(error)
+            res.send(400, error);
+        else {
+            if(req.accepts('html')) {
+                res.render('data', {
+                    objects: results,
+                    collection: req.params.collection
+                });
+            } else {
+                res.set('Content-Type', 'application/json');
+                res.send(200, results);
+            }
+        }
+    });
+});
+
+app.post('/:collection', function(req, res) {
+    var obj = req.body;
+    var collection = req.params.collection;
+
+    collectionDriver.save(collection, obj, function(error, result) {
+        if(error)
+            res.send(400, error);
+        else
+            res.send(201, result);
+    });
+});
+
+app.get('/:collection/:entity', function(req, res) {
+    var params = req.params;
+    var entity = params.entity;
+    var collection = params.collection;
+
+    if(entity) {
+        collectionDriver.get(collection, entity, function(error, results) {
+            if(error)
+                res.send(400, error);
+            else
+                res.send(200, results);
+        });
+    } else {
+        var error = {
+            message: "Bad URL: " + req.url
+        };
+        res.send(400, error);
+    }
+});
+
+app.put('/:collection/:entity', function(req, res) {
+    var params = req.params;
+    var collection = params.collection;
+    var entity = params.entity;
+
+    if(entity) {
+        collectionDriver.update(collection, entity, function(error, result) {
+            if(error)
+                res.send(400, error);
+            else
+                res.send(200, result);
+        });
+    } else {
+        var error = {
+            message: "Bad URL: " + req.url
+        };
+        res.send(400, error);
+    }
+});
+
+app.delete('/:collection/:entity', function(req, res) {
+    var params = req.params;
+    var collection = params.collection;
+    var entity = params.entity;
+
+    if(entity) {
+        collectionDriver.delete(collection, entity, function(error, result) {
+            if(error)
+                res.send(400, error);
+            else
+                res.send(200, result);
+        });
+    } else {
+        var error = {
+            message: "Bad URL: " + req.url
+        };
+        res.send(400, error);
+    }
+});
+
+app.use(function(req, res) {
+    res.render('404', { url: req.url });
+});
+
+// start app
+app.listen(process.env.PORT || 8081, function() {
+
+});
